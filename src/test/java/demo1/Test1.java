a3c6433051aa30f7e4d90e2efa957b36cee8c7a5
Sandeep-maths <dsandeep9994@gmail.com>
2024-08-20 09:22:01 +0530

    push

---
+++ src/test/java/demo1/Test1.java
@@ -0,0 +1,13 @@
+package demo1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test1() {
+		Reporter.log("demo1 test1", true);
+	}
+
+}
---
+++ src/test/java/demo1/Test2.java
@@ -0,0 +1,13 @@
+package demo1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void test2() {
+		Reporter.log("demo1 test2", true);
+	}
+
+}
